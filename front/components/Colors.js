355e5b277c3da7a32f2a0408382b487e8624494d
TheMeteoRain <meteorain.dev@gmail.com>
2017-12-01 10:00:38 +0200

    Add colors for frames and value types

---
+++ front/components/Colors.js
@@ -0,0 +1,16 @@
+const Colors = {
+  ValueTypes: [
+    '#FFFFFF', //#FFFFFF
+    '#8888FF',
+    '#D20000',
+    '#FFFFFF',
+    '#960000',
+    '#366492',
+    '#FFD700',
+    'D02090',
+  ],
+  // normal, magical, rare, unique, gem
+  FrameTypes: ['#C8C8C8', '#8888FF', '#FFFF77', '#AF6025', '#1BA29B'],
+}
+
+export default Colors
